b086a4afb3c9bd5f654302709081a5cde04bf8b5
Adrien0139 <adescoffier@eleve.st-charles.eu>
2025-11-04 15:04:50 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Laboratoire prêt !")
+const nom = "Adrien ESCOFFIER"
+let age = 16
+//nom = "Pacome"
+console.log(nom)
+age = 17
+console.log(age)
+const motDePasseAttendu = "secret123"
+let motDePasseUtilisateur = "12345"
+if ("Comparaison entre motDePasseUtilisateur et motDePasseAttendu") {
+    console.log("Accès autorisé. Bienvenue !");
+} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
